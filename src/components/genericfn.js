0b9cc77f2098ee956316d1d81a5113caade65ba2
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-06-07 14:35:31 +0300

    minor updates

---
+++ src/components/genericfn.js
@@ -0,0 +1,16 @@
+export function addTwoNumbers(num1, num2){
+    let sum = num1 +  num2;
+    return sum; 
+}
+
+export const userObject =(name, age, username, password)=>{
+    return{
+        Name: name,
+        Age: age,
+        Username: username,
+        Password: password
+    }
+}
+
+//react-scripts test --env=jsdom
+
